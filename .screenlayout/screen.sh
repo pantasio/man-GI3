716c685fc777f796c84ed7c92ea03094ec850912
pantasio <hoang.lduc@gmail.com>
2019-04-24 16:49:38 +0700

    add fonts, modified i3/config i3blocks/config

--- .screenlayout/screen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# dday la cau hinh desktop cho ban than. use this with care
-xrandr --output VGA-0 --off --output DVI-I-1 --off --output DVI-I-0 --primary --mode 1920x1080 --pos 0x737 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate right
